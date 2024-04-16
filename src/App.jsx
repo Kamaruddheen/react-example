69207587b5470f5d465c92bc9ced878906c7af9d
Kamar <57263951+Kamaruddheen@users.noreply.github.com>
2024-04-16 20:30:10 +0530

    feat: react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,76 @@
-import React from "react";
-import "./App.css";
-import DeferredValueExample from "./Components/DeferredValueExample/DeferredValueExample";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
+
+import Welcome from "./Components/Basics/Welcome";
+import BorderWrapper from "./Components/Basics/BorderWrapper";
+import Counter from "./Components/Basics/Counter";
+import CounterHooks from "./Components/Basics/CounterHooks";
+import InputBox from "./Components/Basics/Input/InputBox";
 
 function App() {
-  // const [isCounterVisible, setVisible] = useState(true)
+  const [isCounterVisible, setVisible] = useState(true);
   return (
-    <>
-      {/* <BorderWrapper color="red">
-        <Welcome program={"Data Science"} name="Max" />
-      </BorderWrapper>
-      <BorderWrapper color="green">
-        <Welcome program={"Full Stack"} name="Alex" />
-      </BorderWrapper> */}
-      {/* {isCounterVisible && <Counter />} */}
-      {/* <button onClick={() => setVisible(!isCounterVisible)}>hide/show</button> */}
-      {/* <Counter /> */}
-      {/* <CounterHooks /> */}
-      {/* <InputBox /> */}
-      {/* <InputHooks />  */} {/* function based approach with Hooks */}
-      {/* <ReducerExample2 /> */}
-      {/* <Search /> */}
-      <DeferredValueExample />
-    </>
+    <BrowserRouter>
+      <div>
+        <h1>React Example</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/props">Props</Link>
+            </li>
+            <li>
+              <Link to="/hoc">HOC</Link>
+            </li>
+            <li>
+              <Link to="/state-class-components">
+                State basics with class components
+              </Link>
+            </li>
+            <li>
+              <Link to="/state-class-components-with-lifecycle">
+                State basics with class components (Licycle of React)
+              </Link>
+            </li>
+            <li>
+              <Link to="/hooks">Hooks</Link>
+            </li>
+            <li>
+              <Link to="/searching">Search functionality for persons</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Routes>
+        <Route
+          path="/props"
+          element={<Welcome program={"Data Science"} name="Max" />}
+        />
+        <Route
+          path="/hoc"
+          element={
+            <BorderWrapper color="red">
+              <Welcome program={"Data Science"} name="Max" />
+            </BorderWrapper>
+          }
+        />
+        <Route path="/state-class-components" element={<Counter />} />
+        <Route
+          path="/state-class-components-with-lifecycle"
+          element={
+            isCounterVisible && (
+              <>
+                <Counter />
+                <button onClick={() => setVisible(!isCounterVisible)}>
+                  hide/show
+                </button>
+              </>
+            )
+          }
+        />
+        <Route path="/hooks" element={<CounterHooks />} />
+        <Route path="/searching" element={<InputBox />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
